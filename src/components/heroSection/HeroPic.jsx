03360835c5da2c71f20a3f510c0e5fb8c619fd76
Utkarsh Tiwari <153006721+utkxrsh13@users.noreply.github.com>
2025-04-15 12:55:25 +0530

    Update HeroPic.jsx

--- src/components/heroSection/HeroPic.jsx
+++ src/components/heroSection/HeroPic.jsx
@@ -12,7 +12,7 @@ const HeroPic = () => {
       className=" h-full flex items-center justify-center "
     >
       <img
-        src="../../public/images/m2.png"
+        src="/images/m2.png"
         alt="Abdelrahman Qassem"
         className="max-h-[450px] w-auto"
       />
