42f55f47eb39764cfdbdd9734485407b1bab3d9a
Utkarsh Tiwari <utkarsht0813@gmail.com>
2025-04-22 18:22:07 +0530

    cscdsc

--- src/components/projectsSection/ProjectsMain.jsx
+++ src/components/projectsSection/ProjectsMain.jsx
@@ -54,6 +54,7 @@ const ProjectsMain = () => {
               year={project.year}
               align={project.align}
               image={project.image}
+              link = {project.link}
             />
           );
         })}
